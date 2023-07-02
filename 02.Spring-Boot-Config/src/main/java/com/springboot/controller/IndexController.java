970fd5ce5610f3d40736f8405e42d845458b57a1
liual <liual_620@163.com>
2023-07-02 10:24:26 +0800

    feat: 仓库迁移
    <body>
    
    <footer>

---
+++ 02.Spring-Boot-Config/src/main/java/com/springboot/controller/IndexController.java
@@ -0,0 +1,31 @@
+package com.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.bean.BlogProperties;
+import com.springboot.bean.ConfigBean;
+import com.springboot.bean.TestConfigBean;
+
+
+@RestController
+public class IndexController {
+	@Autowired
+	private BlogProperties blogProperties;
+	@Autowired
+	private ConfigBean configBean;
+	@Autowired
+	private TestConfigBean testConfigBean;
+	
+	@RequestMapping("/")
+	String index() {
+		blogProperties.getName();
+		if(blogProperties.getName().endsWith("")){
+			throw new RuntimeException();
+		}
+		return testConfigBean.getName()+"，"+testConfigBean.getAge();
+	}
+}
+
+
